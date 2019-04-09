dbfa9d0ba785f5e0f8ed28b1c658449dc8562291
Pawlllosss <oczadlypawe@gmail.com>
2019-04-09 11:03:41 +0200

    Created routes structure

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const userRouter = require('./user');
 
 router.get('/', function(req, res) {
         res.status(200).send({message: 'Api placeholder'});
 });
+
+router.use('/user', userRouter);
 
 module.exports = router;
