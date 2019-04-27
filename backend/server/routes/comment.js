74749354610d58501572b42d3ca0ca560a04f9e5
Daniel Piskorski <danpisq@gmail.com>
2019-04-27 13:55:23 +0200

    delete and update for comment

--- backend/server/routes/comment.js
+++ backend/server/routes/comment.js
@@ -7,4 +7,8 @@ router.get('/opinion/:opinionId', commentController.retrieveRelatedToOpinion);
 
 router.post('/', commentController.create);
 
+router.delete('/:id', commentController.destroy);
+
+router.put('/:id', commentController.update)
+
 module.exports = router;
