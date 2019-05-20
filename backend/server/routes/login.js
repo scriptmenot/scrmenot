65a719f3a692d7b193feb9485424a3caaebf25ea
Pawlllosss <oczadlypawe@gmail.com>
2019-05-20 22:03:09 +0200

    -removed unnecessary import

--- backend/server/routes/login.js
+++ backend/server/routes/login.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 
 const authenticationController = require('../controllers').Authentication;
 
